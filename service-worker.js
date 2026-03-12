e8c51cf358a02b53835ae5f3801436942c8da89d
torettmrs <torettmrs@gmail.com>
2026-03-12 23:44:15 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,41 @@
-const CACHE = 'continental-v1';
-const FILES = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE = 'continental-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png',
+        './icon-180.png',
+        './icon-32.png'
+      ]);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(cached => {
+        if (cached) return cached;
+        return fetch(e.request).then(response => {
+          cache.put(e.request, response.clone());
+          return response;
+        }).catch(() => cache.match('./index.html'));
+      })
+    )
   );
 });
